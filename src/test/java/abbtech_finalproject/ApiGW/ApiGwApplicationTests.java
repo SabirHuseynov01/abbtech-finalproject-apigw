dbf89646a7fbd369a436b9768e06d0afab23a814
SabirHuseynov01 <huseynovsabir8569@gmail.com>
2025-03-09 22:33:32 +0400

    Hotel Booking System (Api GW)

---
+++ src/test/java/abbtech_finalproject/ApiGW/ApiGwApplicationTests.java
@@ -0,0 +1,13 @@
+package abbtech_finalproject.ApiGW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
